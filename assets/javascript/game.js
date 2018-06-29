39eae41e8dff94d6599b9217d04fc89139b26bb1
Zika Chea <jallsadou@gmail.com>
2018-06-28 21:34:56 -0500

    Commit Messages

---
+++ assets/javascript/game.js
@@ -0,0 +1,72 @@
+
+// global variables
+var wins = 0;
+var losses = 0;
+var counter
+var randomResult;
+var previous = 0;
+
+   //$(".crystal").attr('class', 'red');
+
+var resetAndStart = function() {
+
+    $(".crystals").empty();
+
+    var images =[ "./assets/images/crystal1.jpeg", "assets/images/crystal2.jpeg", "assets/images/crystal3.jpeg", "assets/images/crystal4.jpeg"];
+
+randomResult =Math.floor(Math.random() * 69 ) + 30;
+
+$("#result").html('Random result:' + randomResult);
+
+for ( var i = 0; i < 4; i++) {
+    
+    var random = Math.floor(Math.random() * 11) +1;
+    //console.log(random);
+    
+    var crystal = $("<div>");
+      crystal.attr(images[i]);
+    crystal.attr({"class": 'crystal',
+                  "random-number": random
+                   
+});
+   
+    crystal.css({
+        "background-image ":("" + images[i] + ""),
+        "background-size" :"cover"
+    })
+   
+       // crystal.html(random);
+
+    $(".crystals").append(crystal);
+   
+}
+   $("#previous").html("Total score" + previous);
+}
+  resetAndStart();
+
+
+
+
+  $(document).on('click', ".crystal", function() {
+
+    var num = parseInt($(this).attr('random-number'));
+    previous +=num;
+   console.log(previous);
+   $("#previous").html(previous);
+
+   if (previous > randomResult){
+       losses++;
+       $("#lost").html("You lost" + losses);
+       previous = 0;
+       resetAndStart();
+   }
+   else if (previous === randomResult){
+       wins++;
+       $("#win").html( "You win" + wins);
+       previous = 0;
+       resetAndStart();
+   }
+
+});
+
+  
